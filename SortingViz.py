3883f3dfa510473c5ff7c0c4ddcbbd21da7605b9
Harshal Chamdal <harshalc@mit.edu>
2020-07-21 01:11:38 -0500

    ani gen fix

--- SortingViz.py
+++ SortingViz.py
@@ -11,29 +11,26 @@ def selection_sort(array):
                 min_idx = next_idx
         # once/if the minimum index has been found swap
         array[cur_idx], array[min_idx] = array[min_idx], array[cur_idx]
-    yield from array
+        yield array
 
 
 if __name__ == '__main__':
     # Setup plot and randomly shuffled array of numbers
     fig, ax = plt.subplots()
     ax.set_title('Sorting')
-    N = 300
+    N = 100
     array = [num for num in range(N)]
     random.shuffle(array)
     bars = ax.bar(x=range(len(array)), height=array, width=1, align='edge')
     array_gen = selection_sort(array)
-    bars = list(bars)
-    bars.reverse() # want to sort least to greatest
 
-
-    def update(array_val, plt_bars):  # create the updating func for animation
+    def update(array, plt_bars):  # create the updating func for animation
         """
-        :param array_val: next value of frames in our case it will just be next value in array
+        :param array: next value of frames in our case it will just be a copy of the array
         :param plt_bars: list of bars we are modifying
         """
-        cur_bar = plt_bars.pop()
-        cur_bar.set_height(array_val)
+        for height_val, bar in zip(array, plt_bars):
+            bar.set_height(height_val)
 
-ani = ani.FuncAnimation(plt.gcf(), func=update, fargs=(bars,), frames=array_gen, interval=8, repeat=False)
+ani = ani.FuncAnimation(plt.gcf(), func=update, fargs=(bars,), frames=array_gen, interval=10, repeat=False)
 plt.show()
