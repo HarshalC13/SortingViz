dff6150d3ef85ef97aaf34d10f8a834ff81d594d
Harshal Chamdal <harshalc@mit.edu>
2020-07-20 23:27:10 -0500

    initial

---
+++ SortingViz.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation
+
+fig, ax = plt.subplots()
+ax.set_title('Sorting')
+
+def animate(i):
+
+
+
+if __name__ == '__main__':
+    pass
